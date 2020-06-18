cb6f9af4781a8ed35cb949377d40b94643dc8bc6
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-18 15:59:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,6 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  counter = 0
+  while there_are_still_elements_in_array do
+    yield(array[counter])
 end

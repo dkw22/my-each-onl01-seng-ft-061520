8e2a4090c07741822d2ba548791f18659430a85c
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-18 16:05:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,9 @@
 def my_each(array)# put argument(s) here
   # code here
   counter = 0
-  while there_are_still_elements_in_array do
+  while counter < array.length do
     yield(array[counter])
+    counter += 1
+  end
+  array 
 end
